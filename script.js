56a8e3a3ccaf2e45abfd57715c93267e1af5c24b
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-03 16:31:49 +0200

    Inizio Esercizio

---
+++ script.js
@@ -0,0 +1,7 @@
+//Funzione che mi sostituisca una data parola con xxx
+
+//Funzione che dato un contatore, lo aumenti ogni volta che viene sostituita una parola
+
+//richiesta di un lungo testo come input
+
+//creazione di un array con le parole censurate
